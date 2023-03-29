fb975b03358048257821dd8fa9a1660f5127d2d2
TheUnnoticable <sharpenernewt@gmail.com>
2023-03-28 22:40:17 -0400

    Finished 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0){
+        return "ERROR";
+    }
+    let g = '';
+    for (let i = 0; i < num; i++){
+        g += string;
+    }
+    return g;
 };
 
 // Do not edit below this line
