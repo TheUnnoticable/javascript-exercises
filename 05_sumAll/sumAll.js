075eeb411cfb9963dccafc16a944d1212ef72613
TheUnnoticable <sharpenernewt@gmail.com>
2023-03-28 23:13:23 -0400

    Finished 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
 
+    if (a < 0 || b < 0 || isNaN(a) || isNaN(b) || typeof a === 'string' || typeof b === 'string'){
+        return "ERROR";
+    }
+    let n = Math.abs(b - a);
+    let m = a;
+    if (a > b) {m = b};
+    return m * (n+1) + (n)*(n+1)*0.5;
 };
 
 // Do not edit below this line
